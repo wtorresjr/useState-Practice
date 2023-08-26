6ce3f2ddad0ea724c6d6501557073f71fd2b6f48
Will Torres <114450647+wtorresjr@users.noreply.github.com>
2023-08-26 16:20:34 +0000

    Update 3 files

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,18 +1,16 @@
-import './UseState.css';
+import { useState } from "react";
+import "./UseState.css";
 
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
   return (
-    <div className="state">
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
       <button>Dark</button>
       <button>Light</button>
       <h2>DISPLAY COUNT HERE</h2>
-      <button>
-        Increment
-      </button>
-      <button>
-        Decrement
-      </button>
+      <button>Increment</button>
+      <button>Decrement</button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { useState } from "react";
+import "../src/components/UseState/UseState.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
